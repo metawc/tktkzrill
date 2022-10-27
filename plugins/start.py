64642b27c7f62a313145b80d9f83f4c63095e2fd
metawc <108991838+metawc@users.noreply.github.com>
2022-10-27 08:58:47 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,7 +145,7 @@ async def not_joined(client: Bot, message: Message):
         ],
         [
             InlineKeyboardButton(
-                "ᴊᴏɪɴ ɢʀᴏᴜᴘ sᴇxʏ ʙᴜɴɴʏ",
+                "ᴊᴏɪɴ ɢʀᴏᴜᴘ ᴡᴇᴛ ᴀʀᴇᴀ",
                 url = client.invitelink2),
         ]
     ]
