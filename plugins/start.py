791e51f731a7c2adbf26107bda4d9c3af444e6ca
kdozxx <116815985+kdozxx@users.noreply.github.com>
2022-10-27 20:30:46 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,7 +145,7 @@ async def not_joined(client: Bot, message: Message):
         ],
         [
             InlineKeyboardButton(
-                "ᴊᴏɪɴ ɢʀᴏᴜᴘ ʙʟᴀᴄᴋ ᴄᴀᴛ",
+                "ᴊᴏɪɴ ɢʀᴏᴜᴘ sᴇxʏ ʙᴜɴɴʏ",
                 url = client.invitelink2),
         ]
     ]
